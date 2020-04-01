83abd48f95586d5cc2d66e7466b2c03cee5b3eba
Anish Saha <anishsaha_bec@yahoo.com>
2020-04-01 11:25:08 -0700

    adding description in indexing

--- gatsby-node.js
+++ gatsby-node.js
@@ -61,6 +61,7 @@ exports.createPages = async ({ graphql, actions }) => {
                 tags
                 category
                 date
+                description
               }
             }
           }
