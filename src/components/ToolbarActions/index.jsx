217d122c5e2e77f621208a8e22187038e512d000
Anish Saha <anishsaha_bec@yahoo.com>
2020-04-01 22:03:23 -0700

    Adding search Box

--- src/components/ToolbarActions/index.jsx
+++ src/components/ToolbarActions/index.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
 import UserLinks from "../UserLinks";
 import "./ToolbarActions.scss";
+import Search from "../Search";
 
 class Toolbar extends Component {
   render() {
     const { config } = this.props;
     return (
       <div className="toolbar-actions">
+          <div className="search-container">
+              <Search config={config} />
+          </div>
         <div className="userlinks-container">
           <UserLinks config={config} />
         </div>
+
       </div>
     );
   }
