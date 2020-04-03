f00ef7f7993c18b806069dfc1ae53f95dd9eb95b
Anish Saha <anishsaha_bec@yahoo.com>
2020-04-03 16:19:52 -0700

    Adding basic search functionality

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,21 +1,27 @@
 import React, { Component } from 'react' ;
 import { Link } from 'gatsby' ;
 import "./Search.scss" ;
+import { Form, FormControl, Button, ListGroup, ListGroupItem} from 'react-bootstrap';
+
+
+
 
 class Search extends Component {
+
     state = {
         query: '',
-        results: [],
+        results: []
     }
 
-    render() {
+    /*render() {
         return (
             <div className='search'>
-                <input className='search__input'
+                <SearchIconTag />
+                <input className='run'
                        type='text'
                        value={this.state.query}
                        onChange={this.search}
-                       placeholder={'Search'}
+                       placeholder='Search'
                 />
                 <ul className='search__list'>
                     {this.state.results.map((post) => (
@@ -25,10 +31,28 @@ class Search extends Component {
                                 {post.title}
                             </Link>
                         </li>
-
                     )
                     )}
                 </ul>
+            </div>
+        )
+    }*/
+
+    render() {
+        return (
+            <div className="searchForm">
+                <Form>
+                    <FormControl type="text" placeholder="Search Recipes" className="mr-sm-2"  value={this.state.query} onChange={this.search}/>
+                </Form>
+                <ListGroup variant="flush" className="searchResults">
+                    {this.state.results.map(post => (
+                        <ListGroup.Item>
+                            <li key={post.id}>
+                                <Link to={post.url}>{post.title}</Link>
+                            </li>
+                        </ListGroup.Item>
+                    ))}
+                </ListGroup>
             </div>
         )
     }
