053539960926a8c1eb092d562ac7703021cc0fd1
Anish Saha <anishsaha_bec@yahoo.com>
2020-04-09 16:49:36 -0700

    1.Adding Whatsapp and email options for sharing 2.making changes so that this component is accessible from both post and postpreview page(The If else condition at the top) 3.Removing extra path while generating the url

--- src/components/SocialLinks/index.jsx
+++ src/components/SocialLinks/index.jsx
@@ -5,13 +5,17 @@ import {
   TwitterShareButton,
   TelegramShareButton,
   RedditShareButton,
+    WhatsappShareButton,
+    EmailShareButton,
   FacebookShareCount,
   RedditShareCount,
   FacebookIcon,
   TwitterIcon,
   TelegramIcon,
   LinkedinIcon,
-  RedditIcon
+  RedditIcon,
+    WhatsappIcon,
+    EmailIcon
 } from "react-share";
 import urljoin from "url-join";
 import config from "../../../data/SiteConfig";
@@ -19,9 +23,19 @@ import "./SocialLinks.scss";
 
 class SocialLinks extends Component {
   render() {
-    const { postNode, postPath, mobile } = this.props;
-    const post = postNode.frontmatter;
-    const url = urljoin(config.siteUrl, config.pathPrefix, postPath);
+    const { postNode, postPath, mobile ,postInfo } = this.props;
+      let post ;
+      /*Creating if else condition since this component is getting called from both post.jsx and postpreview.jsx. PostPreview provides
+      postNode whereas post provides postInfo
+*/
+      if (typeof postInfo === 'undefined')
+      {
+          post = postNode.frontmatter;
+      }
+      else { post = postInfo;}
+
+// Removing sitepath from urljoin otherwise giving invalid path
+    const url = urljoin(config.siteUrl,postPath);
     const iconSize = mobile ? 36 : 48;
     const filter = count => (count > 0 ? count : "");
     const renderShareCount = count => (
@@ -39,7 +53,7 @@ class SocialLinks extends Component {
         <TwitterShareButton url={url} title={post.title}>
           <TwitterIcon round size={iconSize} />
         </TwitterShareButton>
-        <FacebookShareButton url={url} quote={postNode.excerpt}>
+        <FacebookShareButton url={url} quote={post.description}>
           <FacebookIcon round size={iconSize} />
           <FacebookShareCount url={url}>
             {count => renderShareCount(count)}
@@ -48,13 +62,20 @@ class SocialLinks extends Component {
         <LinkedinShareButton
           url={url}
           title={post.title}
-          description={postNode.excerpt}
+          description={post.description}
         >
           <LinkedinIcon round size={iconSize} />
         </LinkedinShareButton>
         <TelegramShareButton url={url}>
           <TelegramIcon round size={iconSize} />
         </TelegramShareButton>
+          <WhatsappShareButton url={url} title={post.title}>
+              <WhatsappIcon round size={iconSize} />
+          </WhatsappShareButton>
+          <EmailShareButton url={url} subject={post.title} body="Got this great recipe link:">
+              <EmailIcon round size={iconSize} />
+          </EmailShareButton>
+
       </div>
     );
   }
